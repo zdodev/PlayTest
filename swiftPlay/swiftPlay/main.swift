ea89548b09a401e5a260519e8b698d4cb8fd4087
zdodev <iver0945@gmail.com>
2020-11-06 00:05:01 +0900

    generic

--- swiftPlay/swiftPlay/main.swift
+++ swiftPlay/swiftPlay/main.swift
@@ -24,8 +24,60 @@ func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
 
 // Generic Functions
 
-func swapTwoValuesGeneric<T>(_ a: inout T, _ b: inout T) {
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
     let temporaryA: T = a
     a = b
     b = temporaryA
+}
+
+swapTwoValues(&someInt, &anotherInt)
+
+var someString = "hello"
+var anotherString = "world"
+swapTwoValues(&someInt, &anotherInt)
+
+// Type Parameters
+
+// Naming Type Parameters
+
+// Generic Types
+
+struct IntStack {
+    var items = [Int]()
+    
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Int {
+        items.removeLast()
+    }
+}
+
+struct Stack<Element> {
+    var items = [Element]()
+    
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+
+let fromTheTop = stackOfStrings.pop()
+
+// Extending a Generic Type
+
+extension Stack {
+    var topItem: Element? {
+        items.isEmpty ? nil : items[items.count - 1]
+    }
 }
