3270cf860108ae94b911995a74cf54050dee427a
zdodev <iver0945@gmail.com>
2020-11-05 20:41:48 +0900

    update main.swift

--- swiftPlay/swiftPlay/main.swift
+++ swiftPlay/swiftPlay/main.swift
@@ -1,12 +1,31 @@
-//
-//  main.swift
-//  swiftPlay
-//
-//  Created by Zero DotOne on 2020/11/05.
-//
+// The Problem that Generics Solve
 
-import Foundation
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
 
-print("Hello, World!")
-print("Hello, World!")
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
 
+func swapTwoStrings(_ a: inout String, _ b: inout String) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+// Generic Functions
+
+func swapTwoValuesGeneric<T>(_ a: inout T, _ b: inout T) {
+    let temporaryA: T = a
+    a = b
+    b = temporaryA
+}
