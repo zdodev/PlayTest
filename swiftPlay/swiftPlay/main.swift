869a5f22dda3c79d18eaf682e4562f3f0ec393d7
zdodev <iver0945@gmail.com>
2020-11-10 23:36:07 +0900

    Returning an Opaque Type

--- swiftPlay/swiftPlay/main.swift
+++ swiftPlay/swiftPlay/main.swift
@@ -37,9 +37,72 @@ struct JoinedShape<T: Shape, U: Shape>: Shape {
     var bottom: U
     
     func draw() -> String {
-        return top.draw() + "\n" + bottom.draw()
+        top.draw() + "\n" + bottom.draw()
     }
 }
 
 let joinedTriangle = JoinedShape(top: smallTriangle, bottom: flippedTriangle)
 print(joinedTriangle.draw())
+
+// Returning an Opaque Type
+
+struct Square: Shape {
+    var size: Int
+    
+    func draw() -> String {
+        let line = String(repeating: "*", count: size)
+        let result = Array<String>(repeating: line, count: size)
+        
+        return result.joined(separator: "\n")
+    }
+}
+
+func makeTrapezoid() -> some Shape {
+    let top = Triangle(size: 2)
+    let middle = Square(size: 2)
+    let bottom = FlippedShape(shape: top)
+    let trapezoid = JoinedShape(top: top, bottom: JoinedShape(top: middle, bottom: bottom))
+    
+    return trapezoid
+}
+
+let trapezoid = makeTrapezoid()
+print(trapezoid.draw())
+
+func flip<T: Shape>(_ shape: T) -> some Shape {
+    FlippedShape(shape: shape)
+}
+
+func join<T: Shape, U: Shape>(_ top: T, _ bottom: U) -> some Shape {
+    JoinedShape(top: top, bottom: bottom)
+}
+
+let opaqueJoinedTriangles = join(smallTriangle, flip(smallTriangle))
+print(opaqueJoinedTriangles.draw())
+
+/*
+func invalidFlip<T: Shape>(_ shape: T) -> some Shape {
+    if shape is Square {
+        return shape
+    }
+    
+    return FlippedShape(shape: shape)
+}
+ */
+
+struct FlippedShape1<T: Shape>: Shape {
+    var shape: T
+    
+    func draw() -> String {
+        if shape is Square {
+            return shape.draw()
+        }
+        let lines = shape.draw().split(separator: "\n")
+        
+        return lines.reversed().joined(separator: "\n")
+    }
+}
+
+func `repeat`<T: Shape>(shape: T, count: Int) -> some Collection {
+    Array<T>(repeating: shape, count: count)
+}
