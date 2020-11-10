3ba13f30019a89acdd8775556587b6da04b773eb
zdodev <iver0945@gmail.com>
2020-11-10 23:07:00 +0900

    shape

--- swiftPlay/swiftPlay/main.swift
+++ swiftPlay/swiftPlay/main.swift
@@ -1,2 +1,19 @@
-let a: Int? = 2
-print(a)
+protocol Shape {
+    func draw() -> String
+}
+
+struct Triangle: Shape {
+    var size: Int
+    
+    func draw() -> String {
+        var result = [String]()
+        for length in 1...size {
+            result.append(String(repeating: "*", count: length))
+        }
+        
+        return result.joined(separator: "\n")
+    }
+}
+
+let smallTriangle = Triangle(size: 3)
+print(smallTriangle.draw())
