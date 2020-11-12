3057a7c1d532a0bcf7a7e29d15210ace31460bb5
zdodev <iver0945@gmail.com>
2020-11-13 00:46:01 +0900

    isValidUrl

--- swiftPlay/swiftPlay/main.swift
+++ swiftPlay/swiftPlay/main.swift
@@ -6,17 +6,13 @@ let urlAddress2 = "https//www.naver.com"
 let urlAddress3 = "htps://www.naver.com"
 let urlAddress4 = "https://www.naver.com"
 
-func urlCheck(to invitation: String) {
-//    if let range = invitation.range(of: #"\bClue(do)?™?\b"#, options: .regularExpression) {
-    if let range = invitation.range(of: #"^https?://"#, options: .regularExpression) {
-        print(invitation[range])
-    } else {
-        print("퍼킹")
+func isValidUrl(url: String) {
+    guard let _ = url.range(of: #"^https?://"#, options: .regularExpression) else {
+        print("fail")
+        return
     }
+//    let a = url.range(of: #"^https?://"#, options: .regularExpression)
+//    print(a)
+//    print(url[a])
 }
 
-urlCheck(to: urlAddress)
-urlCheck(to: urlAddress1)
-urlCheck(to: urlAddress2)
-urlCheck(to: urlAddress3)
-urlCheck(to: urlAddress4)
