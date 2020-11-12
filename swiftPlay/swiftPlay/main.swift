ebcb99f591b9f13c2f09e1e23bfe67951a9801be
zdodev <iver0945@gmail.com>
2020-11-12 19:44:01 +0900

    regex

--- swiftPlay/swiftPlay/main.swift
+++ swiftPlay/swiftPlay/main.swift
@@ -1,1 +1,22 @@
+import Foundation
 
+let urlAddress = "https://www.naver.com"
+let urlAddress1 = "http://www.naver.com"
+let urlAddress2 = "https//www.naver.com"
+let urlAddress3 = "htps://www.naver.com"
+let urlAddress4 = "https://www.naver.com"
+
+func urlCheck(to invitation: String) {
+//    if let range = invitation.range(of: #"\bClue(do)?™?\b"#, options: .regularExpression) {
+    if let range = invitation.range(of: #"^https?://"#, options: .regularExpression) {
+        print(invitation[range])
+    } else {
+        print("퍼킹")
+    }
+}
+
+urlCheck(to: urlAddress)
+urlCheck(to: urlAddress1)
+urlCheck(to: urlAddress2)
+urlCheck(to: urlAddress3)
+urlCheck(to: urlAddress4)
