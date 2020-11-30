8b03a992edf1b313b4a0f07b04d92d88ea649c30
zdodev <iver0945@gmail.com>
2020-11-30 22:42:16 +0900

    CaseIterable 테스트

--- test/test/main.swift
+++ test/test/main.swift
@@ -1,21 +1,7 @@
-//
-//  main.swift
-//  test
-//
-//  Created by zdo on 2020/11/28.
-//
-
-import Foundation
-
-print("Hello \(factorial(5))")
-
-func sapi(_ x: Int, arg: CVarArg...) -> Int {
-    return withVaList(arg) { _ in
-        factorial(Int32(x))
-    }
+enum Cookie: CaseIterable {
+    case choco, banana, strawberry
 }
 
-print(sapi(4, arg: "nil"))
-
-//https://developer.apple.com/documentation/swift/imported_c_and_objective-c_apis/using_imported_c_functions_in_swift
-//https://developer.apple.com/documentation/swift/cvararg
+for cookie in Cookie.allCases {
+    print(cookie)
+}
