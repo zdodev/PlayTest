8672962072053a407ca82d93f45fde48480961b8
zdodev <iver0945@gmail.com>
2020-12-05 23:26:57 +0900

    Adopting Common Protocols

--- test/test/main.swift
+++ test/test/main.swift
@@ -1,7 +1,77 @@
-enum Cookie: CaseIterable {
-    case choco, banana, strawberry
+import Foundation
+
+struct Position: Equatable, Hashable {
+    var x: Int
+    var y: Int
+    
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
 }
 
-for cookie in Cookie.allCases {
-    print(cookie)
+let availablePositions = [Position(0, 0), Position(0, 1), Position(1, 0)]
+let gemPosition = Position(1, 0)
+
+for position in availablePositions {
+    if gemPosition == position {
+        print("Gem found at (\(position.x), \(position.y))!")
+    } else {
+        print("No gem at (\(position.x), \(position.y))")
+    }
+}
+
+var visitedPositions: Set = [Position(0, 0), Position(1, 0)]
+let currentPosition = Position(1, 3)
+
+if visitedPositions.contains(currentPosition) {
+    print("Already visited (\(currentPosition.x), \(currentPosition.y))")
+} else {
+    print("First time at (\(currentPosition.x), \(currentPosition.y))")
+    visitedPositions.insert(currentPosition)
+}
+
+class Player {
+    var name: String
+    var position: Position
+    
+    init(name: String, position: Position) {
+        self.name = name
+        self.position = position
+    }
+}
+
+extension Player: Equatable {
+    static func ==(lhs: Player, rhs: Player) -> Bool {
+        lhs.name == rhs.name && lhs.position == rhs.position
+    }
+}
+
+extension Player: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(position)
+    }
+}
+
+class MyNSObjectSubclass: NSObject {
+    let firstProperty = 1
+    let secondProperty = 2
+}
+
+extension MyNSObjectSubclass {
+    override func isEqual(_ object: Any?) -> Bool {
+        guard let other = object as? MyNSObjectSubclass else {
+            return false
+        }
+
+        return self.firstProperty == other.firstProperty && self.secondProperty == other.secondProperty
+    }
+    
+    override var hash: Int {
+        var hasher = Hasher()
+        hasher.combine(firstProperty)
+        hasher.combine(secondProperty)
+        return hasher.finalize()
+    }
 }
