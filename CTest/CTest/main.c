686dc3b20b53418d7989eed78eaecbea7d1930b2
zdodev <iver0945@gmail.com>
2020-12-09 23:05:24 +0900

    feat: whoPrecOp

--- CTest/CTest/main.c
+++ CTest/CTest/main.c
@@ -16,3 +16,15 @@ int getOpPrec(char op) {
     
     return -1;
 }
+
+int whoPrecOp(char op1, char op2) {
+    int op1Prec = getOpPrec(op1);
+    int op2Prec = getOpPrec(op2);
+    
+    if (op1Prec > op2Prec)
+        return 1;
+    else if (op1Prec < op2Prec)
+        return -1;
+    else
+        return 0;
+}
