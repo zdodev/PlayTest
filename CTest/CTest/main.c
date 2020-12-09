02a921aa152772293eccaad91c08b89755e88701
zdodev <iver0945@gmail.com>
2020-12-09 23:01:03 +0900

    create: CTest Project

---
+++ CTest/CTest/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CTest
+//
+//  Created by zdo on 2020/12/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
