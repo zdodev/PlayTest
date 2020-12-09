c2ed2f544ac764a9372096af48c99a8d13d80b4b
zdodev <iver0945@gmail.com>
2020-12-09 22:56:32 +0900

    remove: testc.c

---
+++ test/test/test.c
@@ -0,0 +1,8 @@
+//
+//  test.c
+//  test
+//
+//  Created by zdo on 2020/12/09.
+//
+
+#include "test.h"
---
+++ test/test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  test
+//
+//  Created by zdo on 2020/12/09.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_h */
