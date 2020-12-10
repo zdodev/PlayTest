7a1fe07aa6067a4614dbc11a7443f3496b75a2bc
zdodev <iver0945@gmail.com>
2020-12-11 02:05:36 +0900

    feat: ExpressionSyntaxAnalyzer.swift

---
+++ SwiftTest/SwiftTest/Analyzer/ExpressionSyntaxAnalyzer.swift
@@ -0,0 +1,30 @@
+struct ExpressionSyntaxAnalyzer {
+    func convertInfixToPostfix(tokenization: [Token]) -> [Token] {
+        var postfixExpression = [Token]()
+        var operators = Stack<Operator>()
+        
+        for token in tokenization {
+            if let operand = token as? Operand {
+                postfixExpression.append(operand)
+            } else if let `operator` = token as? Operator {
+                if operators.isEmpty {
+                    operators.push(element: `operator`)
+                } else {
+                    while let previousStoredOperator = operators.peek(), previousStoredOperator >= `operator` {
+                        postfixExpression.append(previousStoredOperator)
+                        _ = operators.pop()
+                    }
+                    operators.push(element: `operator`)
+                }
+            }
+        }
+        
+        while !operators.isEmpty {
+            if let previousStoredOperator = operators.pop() {
+                postfixExpression.append(previousStoredOperator)
+            }
+        }
+        
+        return postfixExpression
+    }
+}
