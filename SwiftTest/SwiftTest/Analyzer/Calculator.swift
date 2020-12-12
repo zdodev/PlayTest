e2b7206af28687d93443c377dff6eb317a53491b
zdodev <iver0945@gmail.com>
2020-12-13 00:15:52 +0900

    10진수 완성

--- SwiftTest/SwiftTest/Analyzer/Calculator.swift
+++ SwiftTest/SwiftTest/Analyzer/Calculator.swift
@@ -1,93 +1,3 @@
-//struct Calculator {
-//    func calculate(expression: [Token]) -> Double {
-//        var operands = Stack<Token>()
-//        var calculateResult: Double = 0
-//
-//        for token in expression {
-//            if let operand = token as? IntegerOperand {
-//                operands.push(element: operand)
-//            } else if let operand = token as? RealNumberOperand {
-//                operands.push(element: operand)
-//            } else if let `operator` = token as? Operator {
-//
-//                var intermediateResult: IntegerOperand?
-//
-//                guard let secondOperand = operands.pop()?.value else { return 0 }
-//                guard let firstOperand = operands.pop()?.value else { return 0 }
-//                print("firstoperand: ", firstOperand, "secondoperand: ", secondOperand)
-//
-//                switch `operator`.value {
-//                case "+":
-//                    intermediateResult = IntegerOperand(value: firstOperand + secondOperand)
-//                case "-":
-//                    intermediateResult = IntegerOperand(value: firstOperand - secondOperand)
-//                case "*":
-//                    intermediateResult = IntegerOperand(value: firstOperand * secondOperand)
-//                case "/":
-//                    intermediateResult = IntegerOperand(value: firstOperand / secondOperand)
-//                default:
-//                    print("error") // error
-//                }
-//
-//                guard let result = intermediateResult else { return 0 }
-//                operands.push(element: result)
-//            }
-//        }
-//
-//        if let result = operands.pop() {
-//            calculateResult = Double(result.value)
-//        }
-//
-//        return Double(calculateResult)
-//    }
-//}
-
-struct Calculation {
-    func calculate(_ lhs: Int, _ rhs: Int, `operator`: Operator.Arithmetic) -> Int {
-        switch `operator` {
-        case .addition:
-            return add(<#T##lhs: Int##Int#>, <#T##rhs: Int##Int#>)
-        case .substraction:
-            subtract(lhs, rhs)
-            
-        default:
-            <#code#>
-        }
-    }
-    
-    func add(_ lhs: Int, _ rhs: Int) -> Int {
-        lhs + rhs
-    }
-    
-    func add(_ lhs: Int, _ rhs: Double) -> Double {
-        Double(lhs) + rhs
-    }
-    
-    func add(_ lhs: Double, _ rhs: Int) -> Double {
-        lhs + Double(rhs)
-    }
-    
-    func add(_ lhs: Double, _ rhs: Double) -> Double {
-        lhs + rhs
-    }
-    
-    func subtract(_ lhs: Int, _ rhs: Int) -> Int {
-        lhs - rhs
-    }
-    
-    func subtract(_ lhs: Int, _ rhs: Double) -> Double {
-        Double(lhs) - rhs
-    }
-    
-    func subtract(_ lhs: Double, _ rhs: Int) -> Double {
-        lhs - Double(rhs)
-    }
-    
-    func subtract(_ lhs: Double, _ rhs: Double) -> Double {
-        Double(lhs) - Double(rhs)
-    }
-}
-
 struct Calculator {
     func calculate(postfixTokenExpression: [Token]) -> Token? {
         var temporaryOperandStorage = Stack<Token>()
@@ -101,59 +11,30 @@ struct Calculator {
             case let operatorToken as Operator:
                 guard let secondToken = temporaryOperandStorage.pop() else { return nil }
                 guard let firstToken = temporaryOperandStorage.pop() else { return nil }
-                var intermediateCalculationResult: Token
+                var intermediateCalculationToken: Token?
                 
                 if let secondOperand = secondToken as? IntegerOperand {
                     if let firstOperand = firstToken as? IntegerOperand {
-                        intermediateCalculationResult = IntegerOperand(value: Calculate.add(secondOperand.value, firstOperand.value))
+                        intermediateCalculationToken = IntegerOperand(value: Calculation.calculate(firstOperand.value, secondOperand.value, operatorToken.value))
+                    } else if let firstOperand = firstToken as? RealNumberOperand {
+                        intermediateCalculationToken = RealNumberOperand(value: Calculation.calculate(firstOperand.value, secondOperand.value, operatorToken.value))
+                    }
+                } else if let secondOperand = secondToken as? RealNumberOperand {
+                    if let firstOperand = firstToken as? IntegerOperand {
+                        intermediateCalculationToken = RealNumberOperand(value: Calculation.calculate(firstOperand.value, secondOperand.value, operatorToken.value))
+                    } else if let firstOperand = firstToken as? RealNumberOperand {
+                        intermediateCalculationToken = RealNumberOperand(value: Calculation.calculate(firstOperand.value, secondOperand.value, operatorToken.value))
                     }
                 }
                 
+                guard let intermediateCalculationResult = intermediateCalculationToken else { return nil }
+                temporaryOperandStorage.push(element: intermediateCalculationResult)
             default:
-                <#code#>
+                return nil
             }
         }
+        
+        guard let calculationResultToken = temporaryOperandStorage.pop() else { return nil }
+        return calculationResultToken
     }
 }
-
-
-//        var operands = Stack<Token>()
-//        var calculateResult: Double = 0
-//
-//        for token in expression {
-//            if let operand = token as? IntegerOperand {
-//                operands.push(element: operand)
-//            } else if let operand = token as? RealNumberOperand {
-//                operands.push(element: operand)
-//            } else if let `operator` = token as? Operator {
-//
-//                var intermediateResult: IntegerOperand?
-//
-//                guard let secondOperand = operands.pop()?.value else { return 0 }
-//                guard let firstOperand = operands.pop()?.value else { return 0 }
-//                print("firstoperand: ", firstOperand, "secondoperand: ", secondOperand)
-//
-//                switch `operator`.value {
-//                case "+":
-//                    intermediateResult = IntegerOperand(value: firstOperand + secondOperand)
-//                case "-":
-//                    intermediateResult = IntegerOperand(value: firstOperand - secondOperand)
-//                case "*":
-//                    intermediateResult = IntegerOperand(value: firstOperand * secondOperand)
-//                case "/":
-//                    intermediateResult = IntegerOperand(value: firstOperand / secondOperand)
-//                default:
-//                    print("error") // error
-//                }
-//
-//                guard let result = intermediateResult else { return 0 }
-//                operands.push(element: result)
-//            }
-//        }
-//
-//        if let result = operands.pop() {
-//            calculateResult = Double(result.value)
-//        }
-//
-//        return Double(calculateResult)
-//}
--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,73 +1,8 @@
-//protocol Calculable {
-//    func add(_ lhs: Int, _ rhs: Int) -> Int
-//}
-//
-//protocol DecimalCalculable: Calculable {
-//    func add(_ lhs: Int, _ rhs: Double) -> Double
-//    func add(_ lhs: Double, _ rhs: Int) -> Double
-//    func add(_ lhs: Double, _ rhs: Double) -> Double
-//}
-//
-//protocol BinaryCalculable: Calculable {
-//}
-//
-//struct DecimalCalculator: DecimalCalculable {
-//    func add(_ lhs: Int, _ rhs: Int) -> Int {
-//        lhs + rhs
-//    }
-//
-//    func add(_ lhs: Int, _ rhs: Double) -> Double {
-//        Double(lhs) + rhs
-//    }
-//
-//    func add(_ lhs: Double, _ rhs: Int) -> Double {
-//        lhs + Double(rhs)
-//    }
-//
-//    func add(_ lhs: Double, _ rhs: Double) -> Double {
-//        lhs + rhs
-//    }
-//}
-//
-//struct Calculator {
-//    var decimalCalculator = DecimalCalculator()
-//    var tokens = Stack<Token>()
-//
-//    init() {}
-//
-//    // 공통
-//    func add(_ lhs: String, _ rhs: String) {
-//
-//    }
-//
-//    func subtract() {}
-//    func multiply() {}
-//
-//    // 10진수 전용
-//    func divide() {}
-//
-//    // 2진수 전용
-//    func logicalANDoperation() {}
-//    func logicalORoperation() {}
-//    func logicalNOToperation() {}
-//    func logicalNORoperation() {}
-//    func logicalXORoperation() {}
-//    func logicalNANDoperation() {}
-//    func bitwiseLeftShiftOperation() {}
-//    func bitwiseRightShiftOperation() {}
-//}
-
-//let ex = ExpressionLexicalAnalyzer()
-//let a = ex.convertExpressionToToken(expression: "5.1+4*3*2=")
-//let ex2 = ExpressionSyntaxAnalyzer()
-//let b = ex2.convertInfixToPostfix(tokenization: a!)
-//let c = Calculator()
-//print(c.calculate(expression: b))
 
 let expressions = [
     "12 + 23 - 34",
     "12 + 23 * 34 - 45",
-    "12.23 + 34 * 45"
+    "12.23 + 34 * -45"
 ]
 
 //for expression in expressions {
@@ -81,23 +16,24 @@ let expressions = [
 
 let a = ExpressionAnalyzer()
 let b = TokenAnalyzer()
-let q = a.convertExpressionToToken(expression: expressions[2])
-let w = b.convertInfixToPostfix(tokenExpression: q!)
+let c = a.convertExpressionToToken(expression: expressions[2])
+let d = b.convertInfixToPostfix(tokenExpression: c!)
+let e = Calculator()
+let f = e.calculate(postfixTokenExpression: d!)
 
-if let c = w {
-    for d in c {
-        print(d)
-        if let e = d as? IntegerOperand {
-            print(e.value)
-        } else if let f = d as? RealNumberOperand {
-            print(f.value)
-        } else if let g = d as? Operator {
-            print(g.value.rawValue)
-        }
-    }
+if let k = f as? RealNumberOperand {
+    print(k.value, 12.23 + 34 * -45)
 }
 
-var aa = Token(priority: .low)
-var bb: Token = IntegerOperand(value: 3)
-
-print(bb is IntegerOperand)
+//if let c = w {
+//    for d in c {
+//        print(d)
+//        if let e = d as? IntegerOperand {
+//            print(e.value)
+//        } else if let f = d as? RealNumberOperand {
+//            print(f.value)
+//        } else if let g = d as? Operator {
+//            print(g.value.rawValue)
+//        }
+//    }
+//}
