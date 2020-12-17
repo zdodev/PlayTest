8e118b80746521dbfb1e1dcb64cec427b6ff4750
zdodev <iver0945@gmail.com>
2020-12-18 02:23:59 +0900

    ✅: Calculator methods

---
+++ iOSTest/iOSTest/Calculator.swift
@@ -0,0 +1,17 @@
+struct Calculator {
+    func add(_ lhs: Int, _ rhs: Int) -> Int {
+        lhs + rhs
+    }
+    
+    func subtract(_ lhs: Int, _ rhs: Int) -> Int {
+        lhs - rhs
+    }
+    
+    func multiply(_ lhs: Int, _ rhs: Int) -> Int {
+        lhs * rhs
+    }
+    
+    func divide(_ lhs: Int, _ rhs: Int) -> Int {
+        lhs / rhs
+    }
+}
--- iOSTest/iOSTest/ViewController.swift
+++ iOSTest/iOSTest/ViewController.swift
@@ -1,21 +1,8 @@
-//
-//  ViewController.swift
-//  iOSTest
-//
-//  Created by zdo on 2020/12/06.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let a = Double("1234.1234")
-        print(a!)
     }
-
-
 }
 
