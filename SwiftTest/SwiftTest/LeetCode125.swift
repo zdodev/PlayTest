4151f2a96afb6808c5da87cea467951df7172a6b
zdodev <iver0945@gmail.com>
2020-12-20 16:18:04 +0900

    LeetCode125

---
+++ SwiftTest/SwiftTest/LeetCode125.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+enum LeetCode125{
+    class Solution {
+        func isPalindrome(_ s: String) -> Bool {
+            var characterList = [Character]()
+            for character in s {
+                if character.isLetter || character.isNumber {
+                    characterList.append(contentsOf: character.lowercased())
+                }
+            }
+            
+            while characterList.count > 1 {
+                if characterList.removeFirst() != characterList.removeLast() {
+                    return false
+                }
+            }
+            
+            return true
+        }
+    }
+    
+    class Solution2 {
+        func isPalindrom(_ s: String) -> Bool {
+            let lowerString = s.lowercased()
+            let alnumString = lowerString.replacingOccurrences(of: "[^a-z0-9]", with: "", options: .regularExpression)
+            let reversedString = String(alnumString.reversed())
+            
+            return alnumString == reversedString
+        }
+    }
+}
