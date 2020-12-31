c69b6c2bd82136bf25766740aa72e285a65cf14e
zdodev <iver0945@gmail.com>
2020-12-31 11:12:41 +0900

    Setting a Default Property Value with a Closure or Function

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,12 +1,3 @@
-struct Point {
-    var x: Int
-    var y: Int
-    
-    mutating func move(_ x: Int, _ y: Int) {
-        self.x = x
-        self.y = y
-    }
-}
-
-let point = Point(x: 1, y: 2)
-point.move(3, 4)
+let a: Int = {
+    1
+}()
