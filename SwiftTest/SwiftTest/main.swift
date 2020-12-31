96667c14fddac55db9d48bd19a707970308af948
zdodev <iver0945@gmail.com>
2020-12-31 09:13:07 +0900

    Cannot use mutating member on immutable value

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,61 +1,12 @@
-import Foundation
-
-// NSObject를 상속하여 오버라이드하는 방식
-
-class Bar: NSObject {
-    override var description: String {
-        return "Bar Description"
-    }
+struct Point {
+    var x: Int
+    var y: Int
     
-    override var debugDescription: String {
-        return "Debug Bar Description"
+    mutating func move(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
     }
 }
 
-// 프로토콜을 채택하는 방식
-
-class Foo: CustomStringConvertible, CustomDebugStringConvertible {
-    let name = "Lucas"
-    let age = 25
-    
-    var description: String {
-        return "Foo Description"
-    }
-    
-    var debugDescription: String {
-        return "Debug Foo Description"
-    }
-//    var debugDescription: String {
-//        let className = type(of: self)
-//        let address = "\(Unmanaged.passUnretained(self as AnyObject).toOpaque())"
-//        return "<\(className): \(address)>"
-//    }
-}
-
-extension CustomDebugStringConvertible {
-//    var debugDescription: String {
-//        let className = type(of: self)
-//        let address = "\(Unmanaged.passUnretained(self as AnyObject).toOpaque())"
-//        var description = "<\(className): \(address), {"
-//        let mirror = Mirror(reflecting: self)
-//        description += mirror.children.compactMap {
-//            let (label, value) = $0
-//            guard let propertyName = label else {
-//                return nil
-//            }
-//            return "\(propertyName): \(value)"
-//        }.joined(separator: ", ")
-//        description += "}>"
-//
-//        return description
-//    }
-}
-
-let bar = Bar()
-let foo = Foo()
-
-print(bar)
-debugPrint(bar)
-
-print(foo)
-debugPrint(foo)
+let point = Point(x: 1, y: 2)
+point.move(3, 4)
