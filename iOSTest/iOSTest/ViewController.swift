3290a95d8f6b0671431d13ac99360dc853479244
zdodev <iver0945@gmail.com>
2021-01-01 17:52:44 +0900

    UI Progamming without Storyboard

--- iOSTest/iOSTest/ViewController.swift
+++ iOSTest/iOSTest/ViewController.swift
@@ -1,8 +1,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        loginButton = UIButton(type: .system)
+        loginButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = .black
+        loginButton.tintColor = .cyan
+        view.addSubview(loginButton)
     }
 }
 
