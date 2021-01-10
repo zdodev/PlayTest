b681391dc90fa21363f3fb570c58980be8b2a580
zdodev <iver0945@gmail.com>
2021-01-10 22:29:20 +0900

    🔥: Remove Swift Package and File

--- iOSTest/iOSTest/SceneDelegate.swift
+++ iOSTest/iOSTest/SceneDelegate.swift
@@ -18,15 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 //        guard let _ = (scene as? UIWindowScene) else { return }
-        guard let windowScene = scene as? UIWindowScene else {
+        guard let _ = scene as? UIWindowScene else {
             return
         }
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
-        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
