00bc90b5094e88678524ab2bd36331875c43c6b6
zdodev <iver0945@gmail.com>
2021-01-26 20:49:06 +0900

    🔥: Remove iOSTest.

--- iOSTest/iOSTest/main.swift
+++
@@ -1,9 +0,0 @@
-//
-//  main.swift
-//  iOSTest
-//
-//  Created by Zero DotOne on 2021/01/14.
-//
-
-import UIKit
-UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
