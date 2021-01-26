2c7bcb25fb66dfdb1efc6833cfa12f95c6dac87c
zdodev <iver0945@gmail.com>
2021-01-26 20:36:41 +0900

    ✨: Networking.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,11 +1,80 @@
-//
-//  main.swift
-//  SwiftTest
-//
-//  Created by Zero DotOne on 2021/01/26.
-//
-
 import Foundation
 
-print("Hello, World!")
+struct Request {
+    func requestItemList(requestURL: String) {
+        guard let url = URL(string: requestURL) else {
+            return
+        }
+        print(url)
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, urlResponse, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let decodedData = try decoder.decode(ItemList.self, from: data)
+                    print(decodedData)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func requestItem(requestURL: String) {
+        guard let url = URL(string: requestURL) else {
+            return
+        }
+        print(url)
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, urlResponse, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let decodedData = try decoder.decode(Item.self, from: data)
+                    print(decodedData)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
+    }
+}
 
+struct ItemList: Decodable {
+    let page: Int
+    let items: [Item]
+}
+
+struct Item: Decodable {
+    let id: Int
+    let title: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let registrationDate: Double
+    let descriptions: String?
+    let images: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case price
+        case currency
+        case stock
+        case discountedPrice = "discounted_price"
+        case thumbnails
+        case registrationDate = "registration_date"
+        case descriptions
+        case images
+    }
+}
+
+let request = Request()
+//request.requestItemList(requestURL: "https://camp-open-market.herokuapp.com/items/1")
+request.requestItem(requestURL: "https://camp-open-market.herokuapp.com/item/30")
+
+Thread.sleep(forTimeInterval: 5)
