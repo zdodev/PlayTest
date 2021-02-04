a66c38a53dec12e134f64e78dedd79a1dbfe08d2
zdodev <iver0945@gmail.com>
2021-02-04 20:34:41 +0900

    ✨: Result Type.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,12 +1,34 @@
+import Foundation
 
-let ab: Int? = 4
-
-var a: Any = ab
-
-if a is Int {
-    print("ee")
-    var b = a as! Int
-    print(b)
+enum NetworkError: Error {
+    case badURL
 }
 
-print("asdf\(a)")
+
+func fetchUnreadCount(from urlString: String, completionHandler: @escaping (Result<Int, NetworkError>) -> Void) {
+    guard let url = URL(string: urlString) else {
+        completionHandler(.failure(.badURL))
+        return
+    }
+    
+    print("Fetching \(url.absoluteString)")
+    completionHandler(.success(5))
+}
+
+fetchUnreadCount(from: "https://hackingwithswift.com") { result in
+    switch result {
+    case .success(let count):
+        print("\(count) unread messages.")
+    case .failure(let error):
+        print(error.localizedDescription)
+    }
+}
+
+fetchUnreadCount(from: "https://hackingwithswift.com") { result in
+    switch result {
+    case .success(let count):
+        print("\(count) unread messages.")
+    case .failure(let error):
+        print(error.localizedDescription)
+    }
+}
