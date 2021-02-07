c037d8f21d021897e979a577ec7b8e57802b58f0
zdodev <iver0945@gmail.com>
2021-02-07 17:30:58 +0900

    🧪: Test isEmpty.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,24 +1,3 @@
-print("ee")
+let a = ""
 
-func test() {
-    
-}
-
-let a = { coin -> Int  in
-    print(coin)
-    return 3
-}("a3")
-
-let b = test()
-print(a)
-print(b)
-
-func test2() -> Int {
-    8
-}
-
-struct Target {
-    let a = test2()
-}
-
-print(Target().a)
+print(a.isEmpty)
