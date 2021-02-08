91273ba7e4d37fe6bc982dcdca973a93d62147fe
zdodev <iver0945@gmail.com>
2021-02-08 21:36:53 +0900

    ⏰: Measurement of execution time.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,11 +1,29 @@
-//
-//  main.swift
-//  SwiftTest
-//
-//  Created by Zero DotOne on 2021/02/08.
-//
-
 import Foundation
 
-print("Hello, World!")
+func measureTime(_ processBlock: () -> Void) {
+    let startTime = Date()
+    processBlock()
+    let intervalTime = Date().timeIntervalSince(startTime)
+    print("running time: \(intervalTime)")
+}
 
+DispatchQueue.global().async {
+    measureTime {
+        var a = 0.1
+        for _ in 1...50000000 {
+            a *= 0.1
+        }
+    }
+}
+
+DispatchQueue.global().async {
+    measureTime {
+        var a = 0
+        for _ in 1...50000000 {
+            a += 1
+        }
+    }
+}
+
+Thread.sleep(forTimeInterval: 1)
+Thread.sleep(forTimeInterval: 20)
