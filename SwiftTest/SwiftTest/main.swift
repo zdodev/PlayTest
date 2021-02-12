45462fc5c5f7b13b4c2d1d4d55238c7fc2c15d86
zdodev <iver0945@gmail.com>
2021-02-12 20:31:10 +0900

    🎉: Begin a project.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,26 +1,21 @@
-let array = [1, 2, 3, 4, 5]
-let multiplication = array.map {
-    $0 + 1
+let arr = [1, 2, 3, 4, 5]
+let map = arr.map {
+    $0 * 2
 }
-print(multiplication)
+print(map)
 
-let array1 = [1, nil, 3, nil, 5, 6, 7]
-let flatMapResult = array1.flatMap {
-    $0
+func add(sum: Int, count: Int) -> Int {
+    var suma = sum
+    suma += count
+    if count > 0 {
+        return add(sum: suma, count: count - 1)
+    } else {
+        return sum
+    }
 }
-let compactMapResult = array1.compactMap {
-    $0
-}
-print(flatMapResult)
-print(compactMapResult)
 
-let array2 = [[1, 2, 3], [nil, 5], [6, nil], [nil, nil]]
-let flatMapResult2 = array2.flatMap {
-    [1] + $0
+let result = arr.reduce(0) {
+    print($0, $1)
+    return $0 + $1
 }
-let compactMapResult2 = array2.compactMap {
-//    print($0)
-    return $0
-}
-//print(flatMapResult2)
-//print(compactMapResult2)
+print(result)
---
+++ iOSTest/iOSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Zero DotOne on 2021/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
