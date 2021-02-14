5a452b94075f6758b58c925449e5e42fe40430e2
zdodev <iver0945@gmail.com>
2021-02-14 22:05:20 +0900

    Test activity indicator.

--- iOSTest/iOSTest/ViewController.swift
+++ iOSTest/iOSTest/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  iOSTest
-//
-//  Created by Zero DotOne on 2021/02/12.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    let activityIndicator: UIActivityIndicatorView = {
+        UIActivityIndicatorView(style: .large)
+    }()
+    
+    let uiView = {
+        UIView()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        view.addSubview(activityIndicator)
+//        activityIndicator.frame = CGRect(x: 50, y: 50, width: 20, height: 20)
+//        view.addSubview(uiView)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+//        uiView.frame = CGRect(x: , y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+        uiView.frame = view.bounds
+        uiView.backgroundColor = .systemRed
+        let window = UIApplication.shared.windows[0]
+//        print(window.safeAreaInsets.)
+//        let uiedge = UIEdgeInsets()
+        print(uiView.frame)
+        print(uiView.bounds)
+    }
 }
 
