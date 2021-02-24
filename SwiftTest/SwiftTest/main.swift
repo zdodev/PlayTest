be07b279ed0fad9ebb8e399a036be0a83cac26a8
zdodev <iver0945@gmail.com>
2021-02-24 15:23:15 +0900

    test.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,21 +1,3 @@
-let arr = [1, 2, 3, 4, 5]
-let map = arr.map {
-    $0 * 2
-}
-print(map)
-
-func add(sum: Int, count: Int) -> Int {
-    var suma = sum
-    suma += count
-    if count > 0 {
-        return add(sum: suma, count: count - 1)
-    } else {
-        return sum
-    }
-}
-
-let result = arr.reduce(0) {
-    print($0, $1)
-    return $0 + $1
-}
-print(result)
+let s = "1 2 3"
+let c = Set(s)
+print(c)
--- iOSTest/iOSTest/ViewController.swift
+++ iOSTest/iOSTest/ViewController.swift
@@ -1,110 +1,80 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    private let button: UIButton = {
-        let button = UIButton(type: .system)
-        return button
-    }()
-    
-    private let searchBar: UISearchBar = {
-        let searchBar = UISearchBar()
-        return searchBar
-    }()
-    
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        return tableView
-    }()
-    
-    private let textView: UITextView = {
-        let textView = UITextView()
-        textView.dataDetectorTypes = .all
-        return textView
-    }()
-    
-    var memoData = [MemoItem]()
+    let splitVC = UISplitViewController(style: .doubleColumn)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 300, height: 70))
+        button.setTitle("Present", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
         view.addSubview(button)
-        view.addSubview(searchBar)
-        view.addSubview(tableView)
-        view.addSubview(textView)
-        
-        tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        guard let dataAsset = NSDataAsset(name: "sample") else {
-            return
-        }
-    
-        do {
-            memoData = try JSONDecoder().decode([MemoItem].self, from: dataAsset.data)
-        } catch {
-            print(error.localizedDescription)
-        }
-        
-        navigationItem.title = "메모"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
-        navigationController?.navigationBar.backgroundColor = .blue
-//        navigationController?.view.
+        button.center = view.center
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
     
-    override func viewDidLayoutSubviews() {
-        switch traitCollection.horizontalSizeClass {
-        case .compact:
-            displayListView()
-        case .regular:
-            displayMemoView()
-        case .unspecified:
-            print("unspecified.")
-        @unknown default:
-            fatalError()
-        }
-        tableView.reloadData()
-    }
-    
-    private func displayListView() {
-        searchBar.frame = CGRect(x: 0, y: view.layoutMargins.top, width: view.frame.width, height: 50)
-        tableView.frame = CGRect(x: 0, y: searchBar.frame.origin.y + searchBar.frame.height, width: view.frame.width, height: view.frame.height - searchBar.frame.origin.y - searchBar.frame.height)
-        textView.isHidden = true
-    }
-    
-    private func displayMemoView() {
-        searchBar.frame = CGRect(x: 0, y: view.layoutMargins.top, width: view.frame.width / 3, height: 50)
-        tableView.frame = CGRect(x: 0, y: searchBar.frame.origin.y + searchBar.frame.height, width: view.frame.width, height: view.frame.height - searchBar.frame.origin.y - searchBar.frame.height)
-        textView.frame = CGRect(x: view.frame.width / 3, y: view.layoutMargins.top, width: view.frame.width - view.frame.width / 3, height: view.frame.height - searchBar.frame.origin.y + searchBar.frame.height)
-        textView.isHidden = false
+    @objc private func didTapButton() {
+        let menuVC = MenuController(style: .plain)
+        menuVC.delegate = self
+        
+        let secondVC = UIViewController()
+        secondVC.title = "Home"
+        secondVC.view.backgroundColor = .systemBlue
+        splitVC.viewControllers = [
+//            UINavigationController(rootViewController: menuVC),
+            menuVC,
+//            UINavigationController(rootViewController: secondVC)
+            secondVC
+        ]
+        present(splitVC, animated: true)
     }
 }
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        memoData.count
+extension ViewController: MenuControllerDelegate {
+    func didTapMenuItem(at index: Int, title: String?) {
+        (splitVC.viewControllers.last as? UINavigationController)?.popViewController(animated: true)
+        
+        let vc = UIViewController()
+        vc.view.backgroundColor = .systemRed
+        vc.title = title
+        
+        (splitVC.viewControllers.last as? UINavigationController)?.pushViewController(vc, animated: true)
+    }
+}
+
+protocol MenuControllerDelegate: AnyObject {
+    func didTapMenuItem(at index: Int, title: String?)
+}
+
+class MenuController: UITableViewController {
+    weak var delegate: MenuControllerDelegate?
+    
+    override init(style: UITableView.Style) {
+        super.init(style: style)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
-        cell.configure(with: memoData[indexPath.row])
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Option \(indexPath.row)"
+        cell.accessoryType = .disclosureIndicator
         return cell
     }
-}
-
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        55
-    }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if traitCollection.horizontalSizeClass == .compact {
-            let memoViewController = MemoViewController()
-            memoViewController.textView.text = memoData[indexPath.row].body
-            navigationController?.pushViewController(memoViewController, animated: true)
-        } else if traitCollection.horizontalSizeClass == .regular {
-            textView.text = memoData[indexPath.row].body
-            textView.resignFirstResponder()
-            searchBar.resignFirstResponder()
-        }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let cellLabel = tableView.cellForRow(at: indexPath)?.textLabel?.text
+        delegate?.didTapMenuItem(at: indexPath.row, title: cellLabel)
     }
 }
