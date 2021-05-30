ef3eb07f6d3987bf706bf56e67dc9f9a9674443d
zdodev <iver0945@gmail.com>
2021-05-30 14:13:45 +0900

    🧪: URL 타입의 파라미터 출력.

--- SwiftTest/SwiftTest/main.swift
+++ SwiftTest/SwiftTest/main.swift
@@ -1,25 +1,10 @@
-struct High {
-    let highMount: Int
-}
+import Foundation
 
-protocol MiddleComponent {
-    var middleMount: Int { get set }
-}
-
-struct FirstMiddle: MiddleComponent {
-    var middleMount: Int
-    var high: High
-}
-
-struct SecondMiddle: MiddleComponent {
-    var middleMount: Int
-    var high: High
-}
-
-struct Low {
-    let lowMount: Int
-    var middle: MiddleComponent
-}
-
-let low = Low(lowMount: 5, middle: FirstMiddle(middleMount: 5, high: High(highMount: 5)))
-print(low)
+let url = URL(string: "https://note.heyo.me/wp-content/uploads/2017/02/stack_memory.png")!
+print(url.absoluteString)
+print(url.absoluteURL)
+print(url.baseURL)
+print(url.host)
+print(url.relativePath)
+print(url.relativeString)
+print(url.fragment)
