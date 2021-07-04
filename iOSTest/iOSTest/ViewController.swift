51bb412ae284d96fe08e6f9cff3999c356044c33
zdodev <iver0945@gmail.com>
2021-07-04 22:22:20 +0900

    UserDefaults 사용

--- iOSTest/iOSTest/ViewController.swift
+++ iOSTest/iOSTest/ViewController.swift
@@ -1,16 +1,4 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    @IBOutlet weak var myView: UIView!
-    var page = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func pressButton(_ sender: UIButton) {
-//        print(view.constraints.first!)
-        let const = view.constraints[1]
-        const.constant += 10
-    }
 }
