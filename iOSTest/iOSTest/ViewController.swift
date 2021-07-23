3b0008e724f97b348b3ef74c4197b8199a6798e0
zdodev <iver0945@gmail.com>
2021-07-23 12:21:43 +0900

    🏳️‍🌈: Localization.

--- iOSTest/iOSTest/ViewController.swift
+++ iOSTest/iOSTest/ViewController.swift
@@ -1,4 +1,9 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        label.text = NSLocalizedString("Hello", comment: "")
+    }
 }
